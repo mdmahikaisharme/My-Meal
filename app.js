f95b7253ed5f8b1b5643778c6571cbfb6e2ad9fb
Md Mahi Kaishar <68140108+mdmahikaisharme@users.noreply.github.com>
2021-03-18 12:37:04 +0600

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@
 // Variable
 let search_text = document.getElementById("search_text");
 let search_btn = document.getElementById("search_btn");
+let back_btn = document.getElementById("back_btn");
 let mainCard = document.getElementById("main");
+let sub_main = document.getElementById("sub_main");
 
 
 // Event Listener
@@ -30,11 +32,12 @@ function clickButton() {
             })
         };
         getMealData(_value.trim());
-
+    } else {
+        alert("For Search You Need To Type!");
     }
     
 };
-function createCard(title, image) {
+function createCard(title, image, ...items) {
     let div = document.createElement("div");
     div.className = "card";
     let p = document.createElement("p");
@@ -43,20 +46,56 @@ function createCard(title, image) {
     img.src = image;
     // Add
     div.appendChild(p);
-    div.appendChild(img);
+    div.appendChild(img);    
+    // Event Listener
+    div.addEventListener("click", divClick.bind(title, image, ...items));
+    // Add
     mainCard.appendChild(div);
-}
+};
+function divClick(title, image, ...items) {
+    mainCard.style.display = "none";
+    sub_main.style.display = "flex";
+    // sub main
+    let div1 = document.createElement("div")
+    div1.className = "card";
+    let p1 = document.createElement("p")
+    p1.innerText = image;
+    let img1 = document.createElement("img")
+    img1.src = title;
+    let ul1 = document.createElement("ul");
+    // Travarsing
+    let _item = [...items].slice(0, 6);
+    _item.forEach(value => {
+        let li1 = document.createElement("li");
+        li1.innerText= value;
+        ul1.appendChild(li1)
+    });
+    div1.appendChild(p1);
+    div1.appendChild(img1);
+    div1.appendChild(ul1);
+    sub_main.appendChild(div1);
+        
+    back_btn.addEventListener("click", () => {
+        mainCard.style.display = "block";
+        mainCard.style.display = "flex";
+        sub_main.style.display = "none";
+        div1.remove();
+    });
+};
+
+
 async function getMealData(nameOfMeal) {
     try {
-        let result = await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=" +nameOfMeal);
+        let result = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" +nameOfMeal);
         let data = await result.json();
         let array = Object.entries(data)[0][1];
         array.forEach(value => {
             let _v =  value['strMeal'].length <= 24 ? value['strMeal']:value['strMeal'].slice(0, 24)+'...';
-            createCard(_v, value['strMealThumb']);
+            createCard(_v, value['strMealThumb'], value['strIngredient1'], value['strIngredient2'], value['strIngredient3'], value['strIngredient4'], value['strIngredient5'], value['strIngredient6'], value['strIngredient7']);
         });
 
     } catch (error) {
+        alert("We Have Nothing To Show You!")
         console.log(error);
     }
 };
